7c86be9d28cf3ccfaf9f80d4664f9ef840c2bb77
Davide Boscariol <gnappoforever@hotmail.it>
2026-01-02 23:04:11 +0100

    Add notes on variables

--- src/test/java/balanceManager/modelTest/AccountTest.java
+++ src/test/java/balanceManager/modelTest/AccountTest.java
@@ -3,19 +3,20 @@ package balanceManager.modelTest;
 import it.BruttiF27.balanceManager.model.Person;
 import it.BruttiF27.balanceManager.model.Transaction;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import it.BruttiF27.balanceManager.model.Account;
-
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows; // anche qui, sempre rispettando la convenzione
 
 public class AccountTest {
 
     // Required fields
-    private final Account accountTest = new Account("familyAccount");
-    private final Person p1 = new Person("Fun", "Guy");;
+    private final Account accountTest = new Account("familyAccount"); // inoltre, sempre per convenzione, solitamente una variabile final che viene instanziata a compile time, non ha senso di non essere anche static, dunque costante
+    private final Person p1 = new Person("Fun", "Guy");; // anche qui, inventati un nome significativo, in questo contesto può essere persino "person"
     private Transaction transactionTest;
 
+    // ma manca il setup della transaction o mi sono perso io?
+    
     @Test // If we give a null name to account, throw exception.
     public void checkNullAccount () { assertThrows(NullPointerException.class, () -> new Account(null)); }
 
@@ -27,7 +28,7 @@ public class AccountTest {
 
     @Test // Checks if the user is trying to add a duplicate member. If so, throws.
     public void checkDuplicateAccountMember () {
-        accountTest.addMember(new Person("Fun", "Guy"));
+        accountTest.addMember(new Person("Fun", "Guy")); // perché non riusi lo stesso riferimento? o alla peggio, crei una nuova variabile locale con gli stessi valori?
         assertThrows(IllegalArgumentException.class, () -> accountTest.addMember(new Person("Fun", "Guy")));
     }
 
